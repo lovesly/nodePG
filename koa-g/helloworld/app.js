8f6fe8867bd0fd4a4a683b026a875203d288f93f
lovesly <zzhlovesly@outlook.com>
2021-01-19 23:08:53 +0800

    feat: 调试配置

--- koa-g/helloworld/app.js
+++ koa-g/helloworld/app.js
@@ -21,7 +21,8 @@ app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
-  extension: 'pug'
+  // extension: 'pug'
+  extension: 'react'
 }))
 
 // logger
